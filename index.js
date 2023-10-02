54ccfde43cb23599a5c821e7cb360d6857ad4889
Ussen_Kimanuka <ukimanuka@gmail.com>
2023-10-02 05:18:13 +0300

    conditionals

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let charges ="I will gladly take your thirty bucks.";
+  let thisOnIsOnMe = "This one is on me!";
+  let not = "No can do.";
+  if (ride > 2500){
+    //let thisOnIsOnMe = "This one is on me!";
+    return not;
+  } else if (ride > 2000){
+    //let charges ="I will gladly take your thirty bucks.";
+    return charges;
+  } else if (ride <= 400){
+    return thisOnIsOnMe;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  //if (city==="NYC"){let ok="Ok, sounds good."; return ok;}
+  let a = "NYC";
+  return (city != a) ? "No go." : "Ok, sounds good.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+  switch (Tip) {
+
+    case 'generous':
+        return Tip = "Thank you so much.";
+        //break;
+
+    case 'not as generous':
+        return Tip = "Thank you.";
+        //break;
+    
+    default:
+        return Tip = "Bye.";
+      //break;
+}
+
 }
